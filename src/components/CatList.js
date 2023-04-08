c89be5ae3d67e5cbc5d54b24d830c6d2fadbe929
Spyros Argyrakos <spyros.argyrakos@gmail.com>
2023-04-08 23:02:06 +0300

    load more

--- src/components/CatList.js
+++ src/components/CatList.js
@@ -6,22 +6,25 @@ const CatList = ({ breedId, showLoadMore = true }) => {
   const [cats, setCats] = useState([]);
   const [selectedCat, setSelectedCat] = useState(null);
   const [, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   const fetchCats = useCallback(async (page) => {
-    const apiUrl = 'https://api.thecatapi.com/v1/images/search';
+    const apiUrl = 'https://api.thecatapi.com/v1/images/search?api_key=live_t2KiIRsSzthnYXP9wZ11pnZHWmTVo3zETISZWqCeAj5IuV8nAcdYEFRnduMeL8vP';
     const params = {
-      limit: 12,
+      limit: 10,
       page,
       order: 'Random',
       breed_id: breedId,
-      headers:{'x-api-key' :'live_t2KiIRsSzthnYXP9wZ11pnZHWmTVo3zETISZWqCeAj5IuV8nAcdYEFRnduMeL8vP'}
     };
 
     try {
+      setLoading(true);
       const response = await axios.get(apiUrl, { params });
       setCats((prevCats) => [...prevCats, ...response.data]);
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   }, [breedId]);
 
@@ -60,8 +63,12 @@ const CatList = ({ breedId, showLoadMore = true }) => {
       </div>
       {selectedCat && <CatModal cat={selectedCat} closeModal={closeModal} />}
       {showLoadMore && (
-        <button className="load-more-cats" onClick={loadMoreCats}>
-          Load More Cats
+        <button
+          className="load-more-cats"
+          onClick={loadMoreCats}
+          disabled={loading}
+        >
+          {loading ? 'Loading...' : 'Load More Cats'}
         </button>
       )}
     </div>
