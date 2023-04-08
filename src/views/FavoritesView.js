bdd57ac9e0fa945b79ac9d587ebc5bb7adcc181b
Spyros Argyrakos <spyros.argyrakos@gmail.com>
2023-04-08 22:00:46 +0300

    views fix

--- src/views/FavoritesView.js
+++ src/views/FavoritesView.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Favorites from '../Favorites';
+import Favorites from '../components/Favorites';
 
 const FavoritesView = () => {
   return (
     <div>
-      <h1>Favorites</h1>
       <Favorites />
     </div>
   );
